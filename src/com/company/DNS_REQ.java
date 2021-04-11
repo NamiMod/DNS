4036363350b3bb1a7015f21ed29874cdd6cabc65
S.NamiModarressi <S.NamiModarressi@gmail.com>
2021-04-11 23:08:13 +0430

    Start

---
+++ src/com/company/DNS_REQ.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * in this class we create and send dns request
+ *
+ *
+ *
+ * Network Project
+ *
+ *
+ * @author Seyed Nami Modarressi
+ * @version 0.1
+ */
+
+public class DNS_REQ {
+}
