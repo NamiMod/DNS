1e37f86eb638af1f9a48103ac04c68b9c60d529f
S.NamiModarressi <S.NamiModarressi@gmail.com>
2021-04-16 17:47:24 +0430

    Final

--- src/com/company/DNS_REQ.java
+++ src/com/company/DNS_REQ.java
@@ -15,7 +15,7 @@ import org.xbill.DNS.Record;
  *
  *
  * @author Seyed Nami Modarressi
- * @version 0.3
+ * @version 1.5
  */
 
 public class DNS_REQ {
@@ -184,118 +184,98 @@ public class DNS_REQ {
      * @throws IOException cant send message
      */
     public String Send_Req_To_DNS_Iterative(String domain) throws IOException {
-        Name name = null;
+        String name;
         String result = null;
-        int type = Type.A, dclass = DClass.IN;
         Message query, response = null;
         Record rec;
-        SimpleResolver res = null;
-        int i = 0;
+        SimpleResolver res;
+        int i;
         int r = 1;
-        int k;
-        int flag = 0;
-        String server = null;
-        ArrayList<String> root_list = new ArrayList<String>();
+        int flag;
+        String server = "198.41.0.4";
         ArrayList<String> list = new ArrayList<String>();
-        root_list.add("198.41.0.4");
-        root_list.add("192.228.79.201");
-        root_list.add("192.33.4.12");
-        root_list.add("199.7.91.13");
-        root_list.add("192.203.230.10");
-        root_list.add("192.5.5.241");
-        root_list.add("192.112.36.4");
-        root_list.add("198.97.190.53");
-        root_list.add("192.36.148.17");
-        root_list.add("192.58.128.30");
-        root_list.add("193.0.14.129");
-        root_list.add("199.7.83.42");
-        root_list.add("202.12.27.33");
+
         i = 0;
-        k = 0;
-        name = Name.fromString("aut.ac.ir", Name.root);
-        while (true)//i<list.size())
-        {
+        name = domain + ".";
+        while (true) {
+
             flag = 0;
             if (r == 1) {
-                if (k < 13) {
-                    server = root_list.get((k++) % 13); // %13 does round robin
-                    r = 0;
-                } else {
-                    System.out.println("Could not resolve");
-                }
-            } else if (r == 0 && i < list.size())
+                r = 0;
+            } else if (i < list.size()) {
                 server = list.get(i++);
-            else {
+            } else {
                 r = 1;
                 continue;
             }
-            //System.out.println("server name selected: "+server);
-            if (server != null)
-                res = new SimpleResolver(server);
-            else
-                res = new SimpleResolver();
 
-            rec = Record.newRecord(name, type, dclass);
+            res = new SimpleResolver(server);
+            rec = Record.newRecord(Name.fromString(name), 1, 1);
             query = Message.newQuery(rec);
-            //System.out.println(query);
             try {
                 response = res.send(query);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            //System.out.println(response);
-            if (response == null)
-                continue;
-            String head = response.getHeader().toString();
-            if (head.contains("status: NOERROR")) {
-                //check status: NOERROR
-                Record[] ans = response.getSectionArray(Section.ANSWER);
-                if (ans.length == 0) {
-                    //System.out.println("answer is null");
 
-                    Record[] records = response.getSectionArray(Section.AUTHORITY);
-                    if (records.length != 0) {
-                        list.clear();
-                        i = 0;
-                        for (int j = 0; j < records.length; j++) {
-                            if (records[j].getType() == Type.SOA) {
-                            } else
-                                list.add(records[j].rdataToString());
-                            //System.out.println(records[j].rdataToString());
-                        }
-                    } else {
-                        r = 1;
-                        continue;
+            Record[] ans = response.getSectionArray(Section.ANSWER);
+            if (ans.length == 0) {
+                Record[] records = response.getSectionArray(Section.AUTHORITY);
+                if (records.length != 0) {
+                    list.clear();
+                    i = 0;
+                    for (Record record : records) {
+                        list.add(record.rdataToString());
                     }
-                } else {// check if answer has ip address
-                    //list.clear();
-                    for (int j = 0; j < ans.length; j++) {
-                        if (ans[j].getType() == Type.A || ans[j].getType() == Type.AAAA) {
-                            //System.out.println("valid ip");
-                            list.clear();
-                            list.add(ans[j].rdataToString());
-                            System.out.println(ans[j].rdataToString());
-                            result = ans[j].rdataToString();
-                            flag = 1;
-                            break;
-                        }
-                        //else if(ans[j].getType()==Type.CNAME)
-                        else {
-                            //list.add(ans[j].rdataToString());
-                            name = Name.fromString(ans[j].rdataToString(), Name.root);
-                            r = 1;
-                            continue;
-                        }
-                        //System.out.println(ans[j].rdataToString());
-                    }
-                    if (flag == 1)
-                        break;
+                } else {
+                    r = 1;
                 }
             } else {
-                continue;
+                for (Record an : ans) {
+                    if (an.getType() == 1) {
+                        list.clear();
+                        list.add(an.rdataToString());
+                        System.out.println(an.rdataToString());
+                        result = an.rdataToString();
+                        flag = 1;
+                        break;
+                    } else {
+                        name = domain + ".";
+                        r = 1;
+                    }
+                }
+                if (flag == 1) {
+                    break;
+                }
             }
-        }//end of while
+
+        }
         return result;
+    }
+
+    /**
+     * read domains from csv file and resolve them
+     */
+    public void Send_From_CSV() throws IOException {
+
+        ArrayList<String> domains = new ArrayList<>();
+        ArrayList<String> ips = new ArrayList<>();
+
+        String row;
+
+        BufferedReader csvReader = new BufferedReader(new FileReader("domain.csv"));
+        while ((row = csvReader.readLine()) != null) {
+            domains.add(row);
+            ips.add(Send_Req_To_DNS_Recursive(row));
+        }
+        csvReader.close();
+
+        FileWriter csvWriter = new FileWriter("domain.csv");
+        for (int i = 0; i < domains.size(); i++) {
+            csvWriter.append(domains.get(i) + " : " + ips.get(i) + "\n");
+        }
+        csvWriter.flush();
+        csvWriter.close();
     }
 
 }
